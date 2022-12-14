7fe5718d55cce8aeb19ac8f9245eb53542d14720
thiru-kavinkumar <117928919+thiru-kavinkumar@users.noreply.github.com>
2022-12-14 07:19:28 +0100

    Update activities.js

--- js/activities.js
+++ js/activities.js
@@ -2,38 +2,43 @@ async function displayCard1() {
     // import json
     const response = await fetch("../data/building.json");
     let data = await response.json();
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.building) {
-        txt += `
-        <div id="building">
-            <h1>`+ d["name"] + `</h1>
-            <span class="adress"> Adress </span> : `+ d["adress"] + `<br>
-            <table>
-                <tr>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                </tr>
-                <tr>
-                    <td>`+ d["schedule"][0]["monday"] + `</td>
-                    <td>`+ d["schedule"][0]["tuesday"] + `</td>
-                    <td>`+ d["schedule"][0]["wednesday"] + `</td>
-                    <td>`+ d["schedule"][0]["thursday"] + `</td>
-                    <td>`+ d["schedule"][0]["friday"] + `</td>
-                    <td>`+ d["schedule"][0]["saturday"] + `</td>
-                    <td>`+ d["schedule"][0]["sunday"] + `</td>
-                </tr>
-            </table>
-            </p>
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Building </h1>`
+    for (let d of data.building){
+        txt +=  `
+        <div >
+                <h1>`+d["name"]+`</h1>
+                <div class="img-div">
+                    <p>
+                        <span > Adress </span> : `+d["adress"]+`<br>
+                    </p>
+                        <table>
+                            <tr>
+                                <th>Monday</th>
+                                <th>Tuesday</th>
+                                <th>Wednesday</th>
+                                <th>Thursday</th>
+                                <th>Friday</th>
+                                <th>Saturday</th>
+                                <th>Sunday</th>
+                            </tr>
+                            <tr>
+                                <td>`+d["schedule"][0]["monday"]+`</td>
+                                <td>`+d["schedule"][0]["tuesday"]+`</td>
+                                <td>`+d["schedule"][0]["wednesday"]+`</td>
+                                <td>`+d["schedule"][0]["thursday"]+`</td>
+                                <td>`+d["schedule"][0]["friday"]+`</td>
+                                <td>`+d["schedule"][0]["saturday"]+`</td>
+                                <td>`+d["schedule"][0]["sunday"]+`</td>
+                            </tr>
+                        </table>
+                    </p>
+                </div>
         </div>
         `;
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
 
 
@@ -42,145 +47,158 @@ async function displayCard2() {
     const response = await fetch("../data/museum.json");
     let data = await response.json();
     console.log(data);
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.museum) {
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Museum </h1>`
+    for (let d of data.museum){
         txt += `
-        <div id="museum">
-            <h1>`+ d["name"] + `</h1>
-            <p>
-            <span class="adress"> Adress </span> : `+ d["adress"] + `<br>
-            <span class="phone"> Phone </span>: `+ d["phone"] + `<br>
-            <table>
-                <tr>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                </tr>
-                <tr>
-                    <td>`+ d["schedule"][0]["monday"] + `</td>
-                    <td>`+ d["schedule"][0]["tuesday"] + `</td>
-                    <td>`+ d["schedule"][0]["wednesday"] + `</td>
-                    <td>`+ d["schedule"][0]["thursday"] + `</td>
-                    <td>`+ d["schedule"][0]["friday"] + `</td>
-                    <td>`+ d["schedule"][0]["saturday"] + `</td>
-                    <td>`+ d["schedule"][0]["sunday"] + `</td>
-                </tr>
-            </table>
-            </p>
+        <div >
+            <h1>`+d["name"]+`</h1>
+            <div class="img-div">
+                <p>
+                    <span > Adress </span> : `+d["adress"]+`<br>
+                    <span > Phone </span>: `+d["phone"]+`<br>
+                    <table>
+                        <tr>
+                            <th>Monday</th>
+                            <th>Tuesday</th>
+                            <th>Wednesday</th>
+                            <th>Thursday</th>
+                            <th>Friday</th>
+                            <th>Saturday</th>
+                            <th>Sunday</th>
+                        </tr>
+                        <tr>
+                            <td>`+d["schedule"][0]["monday"]+`</td>
+                            <td>`+d["schedule"][0]["tuesday"]+`</td>
+                            <td>`+d["schedule"][0]["wednesday"]+`</td>
+                            <td>`+d["schedule"][0]["thursday"]+`</td>
+                            <td>`+d["schedule"][0]["friday"]+`</td>
+                            <td>`+d["schedule"][0]["saturday"]+`</td>
+                            <td>`+d["schedule"][0]["sunday"]+`</td>
+                        </tr>
+                    </table>
+                </p>
+            </div>
         </div>
         `;
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
 
 async function displayCard3() {
     // import json
     const response = await fetch("../data/theater.json");
     let data = await response.json();
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.theater) {
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Theater </h1>`
+    console.log(data)
+    for (let d of data.theater){
         txt += `
-        <div id="theater">
-            <h1>`+ d["name"] + `</h1>
-            <img src="`+ d["images"][0] + `">
-            <img src="`+ d["images"][1] + `<br>
-            <p>
-            <span class="first_opening"> First Opening : </span>: `+ d["first_opening"] + `<br>
-            <span class="capacity"> Capacity : </span>: `+ d["capacity"] + `<br>
-            <span class="adress"> Adress </span> : `+ d["adress"] + `<br>
-            <span class="website"> Website </span>: `+ d["website"] + `<br>
-            <span class="phone"> Phone </span>: `+ d["phone"] + `
-            </p>
+        <div>
+            <h1>`+d["name"]+`</h1>
+            <div class="img-div">
+                <img src="`+d["images"][0]+`">
+                <img src="`+d["images"][1]+`"><br>
+                <p>
+                    <span > First Opening : </span>: `+d["first_opening"]+`<br>
+                    <span > Capacity : </span>: `+d["capacity"]+`<br>
+                    <span > Adress </span> : `+d["adress"]+`<br>
+                    <span > Website </span>: `+d["website"]+`<br>
+                    <span > Phone </span>: `+d["phone"]+`
+                </p>
+            </div>
         </div>
         `;
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
 
 async function displayCard4() {
     // import json
     const response = await fetch("../data/science.json");
     let data = await response.json();
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.science) {
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Science </h1>`
+    for (let d of data.science){
         txt += `
-        <div id="science">
-            <h1>`+ d["name"] + `</h1>
-            <img src="`+ d["images"][0] + `">
-            <img src="`+ d["images"][1] + `<br>
-            <p>
-            <span class="adress"> Adress </span> : `+ d["adress"] + `<br>
-            <span class="website"> Website </span>: `+ d["website"] + `<br>
-            <span class="phone"> Phone </span>: `+ d["phone"] + `
-            <table>
-                <tr>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                </tr>
-                <tr>
-                    <td>`+ d["schedule"][0]["monday"] + `</td>
-                    <td>`+ d["schedule"][0]["tuesday"] + `</td>
-                    <td>`+ d["schedule"][0]["wednesday"] + `</td>
-                    <td>`+ d["schedule"][0]["thursday"] + `</td>
-                    <td>`+ d["schedule"][0]["friday"] + `</td>
-                    <td>`+ d["schedule"][0]["saturday"] + `</td>
-                    <td>`+ d["schedule"][0]["sunday"] + `</td>
-                </tr>
-            </table>
-            </p>
-        </div>`
+        <div>
+            <h1>`+d["name"]+`</h1>
+            <div class="img-div">
+                <img src="`+d["images"][0]+`">
+                <img src="`+d["images"][1]+`"><br>
+                <p>
+                    <span > Adress </span> : `+d["adress"]+`<br>
+                    <span > Website </span>: `+d["website"]+`<br>
+                    <span > Phone </span>: `+d["phone"]+`
+                    <table>
+                        <tr>
+                            <th>Monday</th>
+                            <th>Tuesday</th>
+                            <th>Wednesday</th>
+                            <th>Thursday</th>
+                            <th>Friday</th>
+                            <th>Saturday</th>
+                            <th>Sunday</th>
+                        </tr>
+                        <tr>
+                            <td>`+d["schedule"][0]["monday"]+`</td>
+                            <td>`+d["schedule"][0]["tuesday"]+`</td>
+                            <td>`+d["schedule"][0]["wednesday"]+`</td>
+                            <td>`+d["schedule"][0]["thursday"]+`</td>
+                            <td>`+d["schedule"][0]["friday"]+`</td>
+                            <td>`+d["schedule"][0]["saturday"]+`</td>
+                            <td>`+d["schedule"][0]["sunday"]+`</td>
+                        </tr>
+                    </table>
+                </p>
+            </div>
+        </>`
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
 
 async function displayCard5() {
     // import json
     const response = await fetch("../data/attraction.json");
     let data = await response.json();
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.attraction) {
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Attraction </h1>`
+    for (let d of data.attraction){
         txt += `
-        <div id="attraction">
-        <h1>`+ d["name"] + `</h1>
-        <p>
-        <span class="adress"> Adress </span> : `+ d["adress"] + `<br>
-        <table>
-            <tr>
-                <th>Monday</th>
-                <th>Tuesday</th>
-                <th>Wednesday</th>
-                <th>Thursday</th>
-                <th>Friday</th>
-                <th>Saturday</th>
-                <th>Sunday</th>
-            </tr>
-            <tr>
-                <td>`+ d["schedule"][0]["monday"] + `</td>
-                <td>`+ d["schedule"][0]["tuesday"] + `</td>
-                <td>`+ d["schedule"][0]["wednesday"] + `</td>
-                <td>`+ d["schedule"][0]["thursday"] + `</td>
-                <td>`+ d["schedule"][0]["friday"] + `</td>
-                <td>`+ d["schedule"][0]["saturday"] + `</td>
-                <td>`+ d["schedule"][0]["sunday"] + `</td>
-            </tr>
-        </table>
-        </p>
+        <div >
+            <h1>`+d["name"]+`</h1>
+            <div class="img-div">
+                <p>
+                <span > Adress </span> : `+d["adress"]+`<br>
+                <table>
+                    <tr>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                    </tr>
+                    <tr>
+                        <td>`+d["schedule"][0]["monday"]+`</td>
+                        <td>`+d["schedule"][0]["tuesday"]+`</td>
+                        <td>`+d["schedule"][0]["wednesday"]+`</td>
+                        <td>`+d["schedule"][0]["thursday"]+`</td>
+                        <td>`+d["schedule"][0]["friday"]+`</td>
+                        <td>`+d["schedule"][0]["saturday"]+`</td>
+                        <td>`+d["schedule"][0]["sunday"]+`</td>
+                    </tr>
+                </table>
+            </p>
+        </div >
     </div>`;
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
 
 
@@ -188,63 +206,24 @@ async function displayCard6() {
     // import json
     const response = await fetch("../data/street.json");
     let data = await response.json();
-    let el = document.getElementById("text_section_activities");
-    let txt = ``
-    for (let d of data.streets) {
+    let el = document.getElementById("text_section_activities"); 
+    let txt = `<h1> Street </h1>`
+    for (let d of data.streets){
         txt += `
-        <div id="street">
-            <h1>`+ d["name"] + `</h1>
-            <img src="`+ d["images"] + `">
+        <div ">
+            <h1>`+d["name"]+`</h1>
+            <div class="img-div">
+                <img src="`+d["images"]+`">
+            
             <p>
-            <span class="length"> Length : </span>: `+ d["length"] + `<br>
-            <span class="width"> Width : </span>: `+ d["width"] + `<br>
-            <span class="feature"> Features : </span>: `+ d["features"] + `<br>
+                <span > Length : </span>: `+d["length"]+`<br>
+                <span > Width : </span>: `+d["width"]+`<br>
+                <span > Features : </span>: `+d["features"]+`<br>
             </p>
+            </div">
         </div>
         `;
     }
     el.innerHTML = txt;
+    window.location.href = "#text_section_activities";
 }
-
-
-function onLoad() {
-    var url = new URL(window.location.href);
-    var title = url.searchParams.get("title");
-    const arr = ["Building",
-        "Museum",
-        "Theater",
-        "Science and technology",
-        "Attraction",
-        "Famous street"]
-    switch (title) {
-        case arr[0]:
-            displayCard1()
-            break;
-        case arr[1]:
-            displayCard2()
-            break;
-        case arr[2]:
-            displayCard3()
-            break;
-        case arr[3]:
-            displayCard4()
-            break;
-        case arr[4]:
-            displayCard5()
-            break;
-        case arr[5]:
-            displayCard6()
-            break;
-        default:
-            break;
-    }
-}
-
-function getDirectory() {
-    var loc = window.location.href;
-    var dir = loc.substring(0, loc.lastIndexOf('/'));
-    dir = loc.substring(0, dir.lastIndexOf('/'));
-    return dir
-}
-
-onLoad()
